e99be67c604b2d3c3e4e6b2f7a78f5967abc675f
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2020-11-27 19:34:36 +0900

    Second_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles # all route ,only: [:show, :index, :new, :create, :edit, :update, :destroy] # resourceが先
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  get 'about', to: 'pages#about'
 end
