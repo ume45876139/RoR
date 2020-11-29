9ad1289aacbe5329a2691e73b0e1981c9638b1c6
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2020-11-29 21:58:08 +0900

    Add_signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,17 +25,22 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, You have saccessfully signup"
+      redirect_to articles_path
+    else
+      render 'new'
     end
   end
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -69,6 +74,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
